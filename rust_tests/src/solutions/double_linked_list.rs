85482d441676ac17c06e9e0292318eb72570610d
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-03-28 23:28:47 +0200

    Refactor trait

--- rust_tests/src/solutions/double_linked_list.rs
+++ rust_tests/src/solutions/double_linked_list.rs
@@ -1,12 +1,14 @@
 pub trait DoubleLinkedList<T> {
     type Node;
-    fn insert_after(node: Node, value: T)->Node;
-    fn insert_before(node: Node, value: T)->Node;
-    fn push_back(value: T)->Node;
-    fn push_top(value: T)->Node;
-    fn delete(node: Node);
-    fn next(node: Node)->Option<Node>;
-    fn prec(node: Node)->Option<Node>;
-    fn first()->Option<Node>;
-    fn last()->Option<Node>;
+    fn insert_after(&mut self, node: Node, value: T)->Node;
+    fn insert_before(&mut self, node: Node, value: T)->Node;
+    fn push_back(&mut self, value: T)->Node;
+    fn push_top(&mut self, value: T)->Node;
+    fn delete(&mut self, node: Node);
+    fn next(&self, node: Node)->Option<Node>;
+    fn prec(&self, node: Node)->Option<Node>;
+    fn first(&self)->Option<Node>;
+    fn last(&self)->Option<Node>;
+    fn value(&self, node: Node)->Option<&T>;
+    fn value_mut(&mut self, node: Node)->Option<&mut T>;
 }
