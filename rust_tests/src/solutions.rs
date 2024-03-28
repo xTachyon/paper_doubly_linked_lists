156a484ec0b717f85a12499085252ef7badd20f8
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-03-28 23:55:18 +0200

    update on trait

--- rust_tests/src/solutions.rs
+++ rust_tests/src/solutions.rs
@@ -1,3 +1,4 @@
+pub mod double_linked_list;
 pub mod handle_impl;
 pub mod index_impl;
 pub mod nonnull_impl;
@@ -5,3 +6,5 @@ pub mod rc_impl;
 pub mod slotmap_impl;
 pub mod std_linked_list_impl;
 pub mod std_map_impl;
+
+use double_linked_list::DoubleLinkedList;
--- rust_tests/src/solutions/double_linked_list.rs
+++ rust_tests/src/solutions/double_linked_list.rs
@@ -1,14 +1,15 @@
 pub trait DoubleLinkedList<T> {
     type Node;
-    fn insert_after(&mut self, node: Node, value: T)->Node;
-    fn insert_before(&mut self, node: Node, value: T)->Node;
-    fn push_back(&mut self, value: T)->Node;
-    fn push_top(&mut self, value: T)->Node;
-    fn delete(&mut self, node: Node);
-    fn next(&self, node: Node)->Option<Node>;
-    fn prec(&self, node: Node)->Option<Node>;
-    fn first(&self)->Option<Node>;
-    fn last(&self)->Option<Node>;
-    fn value(&self, node: Node)->Option<&T>;
-    fn value_mut(&mut self, node: Node)->Option<&mut T>;
+    fn new(capacity: usize)->Self;
+    fn insert_after(&mut self, node: Self::Node, value: T) -> Self::Node;
+    fn insert_before(&mut self, node: Self::Node, value: T) -> Self::Node;
+    fn push_back(&mut self, value: T) -> Self::Node;
+    fn push_top(&mut self, value: T) -> Self::Node;
+    fn delete(&mut self, node: Self::Node);
+    fn next(&self, node: Self::Node) -> Option<Self::Node>;
+    fn prec(&self, node: Self::Node) -> Option<Self::Node>;
+    fn first(&self) -> Option<Self::Node>;
+    fn last(&self) -> Option<Self::Node>;
+    fn value(&self, node: Self::Node) -> Option<&T>;
+    fn value_mut(&mut self, node: Self::Node) -> Option<&mut T>;
 }
