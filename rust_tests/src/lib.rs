44c31ada79d62e9fcb5931a288f656dcba9fc94b
Gavrilut Dragos <gavrilut.dragos@gmail.com>
2024-05-20 23:05:02 +0300

    raw ptr implementation

--- rust_tests/src/lib.rs
+++ rust_tests/src/lib.rs
@@ -75,6 +75,7 @@ pub unsafe extern "C" fn load_tests() -> RawLoadResult {
         list_impl!(slotmap_impl),
         list_impl!(nonnull_impl),
         list_impl!(index_impl),
+        list_impl!(raw_impl),
         // sol!(index_impl),
         // sol!(nonnull_impl),
         // sol!(rc_impl),
--- rust_tests/src/solutions.rs
+++ rust_tests/src/solutions.rs
@@ -2,6 +2,7 @@ pub mod double_linked_list;
 pub mod handle_impl;
 pub mod index_impl;
 pub mod nonnull_impl;
+pub mod raw_impl;
 // pub mod rc_impl;
 pub mod slotmap_impl;
 pub mod std_linked_list_impl;
